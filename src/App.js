da9e2985ff5cf17ae1209b1ed5a260e51a7744d3
BradleyyKim <win930323@gmail.com>
2021-05-25 18:24:05 +0900

    studied props

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./Components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='React course'/>
+      <Todo text='React second'/>
+      <Todo text='React master'/>
+    </div>
+  );
+}
+
+export default App;
