4636392d724b92bbb2632aa02454bfffbe49a4b9
BradleyyKim <win930323@gmail.com>
2021-05-26 14:24:23 +0900

    tutorial studied

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import Todo from "./Components/Todo";
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation"
+//import Todo from "./components/Todo";
 
 function App() {
+  //localhost:3000/products
+  //my-page.com /
+
   return (
     <div>
-      <h1>My Todos</h1>
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+      {/* <h1>My Todos</h1>
       <Todo text='React course'/>
       <Todo text='React second'/>
-      <Todo text='React master'/>
+      <Todo text='React master'/> */}
     </div>
   );
 }
