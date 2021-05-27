2a92c8a99d3c893993de3cfd765d746b3d857c54
BradleyyKim <win930323@gmail.com>
2021-05-27 16:45:52 +0900

    meetup demo file complete

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,26 @@
+import { useHistory } from 'react-router-dom';
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    fetch('https://react-portfolio-a914d-default-rtdb.firebaseio.com/meetups.json',
+    {
+      method: 'POST',
+      body: JSON.stringify(meetupData),
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    }
+    ).then(() => {
+      history.replace('/');
+    });
+  }
     return ( 
         <section>
         <h1>New Meetup Page</h1>
-        <NewMeetupForm />
+        <NewMeetupForm onAddMeetup={addMeetupHandler} />
         </section>
     );
 }
